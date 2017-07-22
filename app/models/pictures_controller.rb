97f70e1e40ecfd9bf2a5167f9e0f8a8463c3638c
Tim Schulte <tschult0@chicagobooth.edu>
2017-07-22 19:33:40 +0000

    golden7_attempt 7

--- app/models/pictures_controller.rb
+++ app/models/pictures_controller.rb
@@ -18,8 +18,6 @@ class PicturesController < ApplicationController
 
     def index
         @all_pics = Photo.all
-        
-        # fortune teller give examples about how to present each
         render("pic_templates/index.html.erb")
     end
 
@@ -34,36 +32,34 @@ class PicturesController < ApplicationController
     def edit_form
         an_id_number = params["an_id"]
         @pic = Photo.find(an_id_number)
-        
-        
+        @pic.save
         render("pic_templates/edit_form.html.erb")
     end
     
     
     def update_row
-        #   Parameters: {"image_url"=>"https://upload.wikimedia.org/wikipedia/commons/
-        #   thumb/9/96/Pair_of_Merops_apiaster_feeding.jpg/640px-Pair_of_Merops_apiaster_feeding.jpg", "caption"=>"Birds3", "some_id"=>"5"}
-          
-          
         the_id_number = params["some_id"]
         p = Photo.find(the_id_number)
         p.source = params["image_url"]
         p.caption = params["caption"]
         p.save
-        
-        
         render("pic_templates/update_row.html.erb")
     end
     
 
     def destroy_row
-        get("/delete_photo/:toast_id", { :controller => "pictures", :action => "destroy_row" })
-        delete_id = params["toast_id"]
-        d = Photo.find(delete_id)
+        # get("/delete_photo/:toast_id", { :controller => "pictures", :action => "destroy_row" })
+        # delete_id = params["toast_id"]
+        # d = Photo.find(delete_id)
+        # d.save
+        # render("pic_templates/destroy_row.html.erb")
         
-        d.save
-        
+        p = Photo.find(params[:toast_id])
+        p.destroy
+        p.save
+        @remaining_count = Photo.count
         render("pic_templates/destroy_row.html.erb")
+
     end
 end
 
@@ -72,20 +68,11 @@ end
 
 
 
-# class PicturesController < ApplicationController
-#   def new_form
-#     render("pic_templates/new_form.html.erb")
-#   end
 
-#   def create_row
-#     p = Photo.new
 
-#     p.source = params[:the_source]
-#     p.caption = params[:the_caption]
 
-#     p.save
 
-#     @current_count = Photo.count
+
 
 #     render("pic_templates/create_row.html.erb")
 #   end
